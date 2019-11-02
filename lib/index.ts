0e4dc764d356e515b9bd06de452497be58b0f039
梁柱 <ymblender@gmail.com>
2019-11-02 17:26:44 +0800

    3

--- lib/index.ts
+++ lib/index.ts
@@ -2,16 +2,32 @@ import { IProps, IStyle } from './interface';
 
 export { IProps, IStyle };
 
-const style = (target: any, obj: IStyle) => {
+const style = <T>(target: T, obj: IStyle): T => {
   Object.keys(obj).forEach(k => {
-    target.style[k] = obj[k];
+    (target as any).style[k] = obj[k];
   });
+
+  return target;
 };
 
-style.css = (text: string) => {
+style.css = (text: string, BEM?: string) => {
   const ele = document.createElement('style');
   ele.innerText = text;
-  document.head.append(ele);
+  ele.type = 'text/css';
+
+  if (BEM) {
+    text = text.replace(/\.\^/g, `.${BEM}-`);
+  }
+  document.head.appendChild(ele);
+};
+
+style.class = <T>(target: T, className: string, BEM?: string): T => {
+  if (BEM) {
+    className = className.replace(/\^/g, `${BEM}-`);
+  }
+  (target as any).setAttribute('class', className);
+
+  return target;
 };
 
 style.sheet = (obj: IStyle) => obj;
